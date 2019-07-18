c81ca8d5727a62a6f5a80654169f0dc3895f6b04
xiacijie <cijie@ualberta.ca>
2019-07-18 00:55:34 -0600

    second sol

--- 0003.py
+++ 0003.py
@@ -12,3 +12,32 @@ class Solution:
                 del hash_map[s[i]]
                 i+= 1
         return ans
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        max = 0
+        for i in range(len(s)):
+            hash = {}
+            terminate = True
+
+            for j in range(i,len(s)):
+
+                if s[j] not in hash.keys():
+                    hash[s[j]] = 1
+                else:
+                    terminate = False
+                    break
+            if terminate:
+                l = j-i+1
+            else:
+                l = j-i
+            if max < l:
+                max = l
+
+                i = j+1
+        return max
+
+
+
+
